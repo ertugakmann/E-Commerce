8bdf08582c509a60ab40232579ec59cb355975f5
Ertuğ Akman <ertug3470@gmail.com>
2024-06-12 18:41:25 +0300

    tailwind loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-blue-300">hello world</h1>
+    </div>
+  );
 }
 
 export default App;
