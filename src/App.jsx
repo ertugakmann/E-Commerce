d3467aeeb4c6cf574ef2b05d999138cc8dc89585
Ertuğ Akman <ertug3470@gmail.com>
2024-06-12 21:17:38 +0300

    header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import PageContainer from "./container/PageContainer";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <PageContainer>
+        <Header />
+      </PageContainer>
+    </div>
+  );
 }
-
 export default App;
---
+++ src/container/PageContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PageContainer({ children }) {
+  return <div className="container mx-auto ">{children}</div>;
+}
+
+export default PageContainer;
