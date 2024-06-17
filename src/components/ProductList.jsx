41fde787797c5921fe36c40b93150b5252b0b104
Ertuğ Akman <ertug3470@gmail.com>
2024-06-17 18:29:32 +0300

    new component created for list products

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProducts } from "../redux/slices/productSlice";
+
+function ProductList() {
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.product);
+
+  console.log(products);
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+  }, []);
+
+  return <div>ProductList</div>;
+}
+
+export default ProductList;
